adb207723d8235c04ef250664d8beaa04da35cf4
Nana-Afia <naowusuachiaw@yahoo.com>
2023-01-24 06:15:50 -0800

    tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""Define a class square"""
 
 class Square:
-    """Defines a square"""
+    """Represents a square"""
+
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
+        """Initialises the data
+        Args:
+            size (int): The size of the new square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
