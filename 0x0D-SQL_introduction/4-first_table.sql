a28d120e323befd555c15a36368e009f6e80ad8d
Nana-Afia <naowusuachiaw@yahoo.com>
2023-03-16 09:02:26 -0700

    task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
 -- creating a table in the current database
-CREATE TABLE first_table IN hbtn_0c_0 (
-	id,
-	name,
-);
-WHERE id = INT AND name = VARCHAR(256);
+CREATE TABLE first_table IN hbtn_0c_0 (id INT), (name VARCHAR(256));
